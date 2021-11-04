f77c8fe918a676cc31357663bfe9619a03686a63
unknown <alfahrimudzik@gmail.com>
2021-11-04 11:26:39 +0700

    pwa config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-module.exports = {
+const withPWA = require("next-pwa");
+module.exports = withPWA({
   images: {
     domains: ["i.ibb.co", "ibb.co"],
   },
   env: {
     publishableKey: `${process.env.STRIPE_PUBLIC_KEY}`,
   },
-};
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,95 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <meta name="application-name" content="wefootwear Store" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="wefootwear Store" />
+          <meta name="description" content="Shoe Store Based in Indonesia" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/static/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#fff" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link
+            rel="apple-touch-icon"
+            href="/static/icons/touch-icon-iphone.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/static/icons/touch-icon-ipad-retina.png"
+          />
+
           <link
             rel="icon"
-            href="https://i.ibb.co/HqH4GXF/logowft.png"
-            type="image/x-icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/icons/favicon-32x32.png"
           />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/static/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://wefootwear.vercel.com" />
+          <meta name="twitter:title" content="wefootwear Store" />
+          <meta
+            name="twitter:description"
+            content="Shoe Store based in Indonesia"
+          />
+          <meta
+            name="twitter:image"
+            content="https://wefootwear.vercel.com/static/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="wefootwear Store" />
+          <meta
+            property="og:description"
+            content="Shoe Store based in Indonesia"
+          />
+          <meta property="og:site_name" content="wefootwear" />
+          <meta property="og:url" content="https://wefootwear.vercel.com" />
+          <meta
+            property="og:image"
+            content="https://wefootwear.vercel.com/static/icons/apple-touch-icon.png"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/icon-512x512.png" type="image/x-icon" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
